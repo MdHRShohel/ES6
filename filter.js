6b5579dfcb1cfdc8e9cfe512e8ed4a42b86ed9f9
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-08-27 02:28:29 +0600

    more example added

--- filter.js
+++ filter.js
@@ -4,3 +4,13 @@ const bigNums = numbers.filter(number => number > 40);
 const tiny = numbers.filter(n => n<40);
 console.log(bigNums);
 console.log(tiny);
+
+
+const products = [
+    {id: 1, name: 'laptop', price: 45000},
+    {id: 2, name: 'mobile', price: 55000},
+    {id: 3, name: 'tablet', price: 34500},
+    ];
+
+const expensive = products.filter(product => product.price > 40000);
+console.log(expensive);
