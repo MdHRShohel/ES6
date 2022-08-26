0db9742d1edf502481c1e7e7cac8521667287365
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-08-27 02:25:34 +0600

    filter example added

--- filter.js
+++ filter.js
@@ -1,1 +1,6 @@
 const numbers = [12, 43, 32, 45, 56, 54, 34, 36];
+
+const bigNums = numbers.filter(number => number > 40);
+const tiny = numbers.filter(n => n<40);
+console.log(bigNums);
+console.log(tiny);
